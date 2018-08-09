315b34469508cf9583bce63ec57dcd627afd068d
leonardo.mlmendes <Leonardo.mlmendes@gmail.com>
2018-08-09 17:14:24 -0300

    Initial commit

---
+++ app/src/main/java/com/example/leonardomlmendes/autenticacao/BancoActivity.java
@@ -0,0 +1,13 @@
+package com.example.leonardomlmendes.autenticacao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BancoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.banco_layout);
+    }
+}
